09616cf2ad188e137261a54962476f47a00fb5fc
Ragnar Westad <ragnar.westad@nav.no>
2018-05-08 15:51:58 +0200

    First!

---
+++ scripts/mock-api-server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const allowCrossDomain = (req, res, next)  => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.raw());
+
+const port = process.env.PORT || 3002;
+const router = express.Router();
+
+router.get('/hello', function (req, res) {
+  return res.json('Hello Ragnar Westad')
+});
+
+app.use(allowCrossDomain);
+app.use('/api', router);
+
+app.listen(port);
