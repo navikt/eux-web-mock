8d57fe8e1618c2acf65ce9566ca3a49002a21a23
Bjølseth, Are <are.bjolseth@nav.no>
2018-06-06 13:42:16 +0200

    EUX-600 enkel mocking av [POST] /api/vedlegg med forskjellig response status [200, 401, 500]

--- scripts/modules/vedlegg.js
+++ scripts/modules/vedlegg.js
@@ -1,11 +1,10 @@
 /*
 const fs = require('fs');
-const ERR = require('./errors');
-const happy = require('./happystatus');
 const MOCK_DATA_DIR = `${process.cwd()}/scripts/mock_data`;
 */
 const utils = require('./utils');
-
+const ERR = require('./errors');
+const happy = require('./happystatus');
 /**
  * Send vedlegg
  * @param req
@@ -15,5 +14,21 @@ const utils = require('./utils');
 exports.send = (req, res) => {
   const body = req.body;
   const responseBody = utils.isJSON(body) ? JSON.parse(body) : body;
-  return res.json(responseBody);
+
+  const status = happy.happyStatus([200, 200, 200, 401, 500]);
+
+  switch (status) {
+    case 200:
+      return res.json(responseBody);
+    case 401: {
+      const melding = ERR.unauthorizedRequest401(req.url);
+      return res.status(status).send(melding);
+    }
+    case 500: {
+      const melding = ERR.serverError500(req.urll);
+      return res.status(status).send(melding);
+    }
+    default:
+      return res.json(responseBody);
+  }
 };
