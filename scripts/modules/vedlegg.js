5e9371aa0f5e8f5b039e5b18d15004c8e4cd7617
Ragnar Westad <ragnar.westad@nav.no>
2018-06-08 14:41:29 +0200

    EUX-600 Litt opprydding i respons-håndteringen

--- scripts/modules/vedlegg.js
+++ scripts/modules/vedlegg.js
@@ -16,21 +16,22 @@ exports.send = (req, res) => {
 
   try {
     let jsonBody = utils.isJSON(body) ? JSON.parse(body) : body;
-    console.log('sendToRina:', jsonBody);
-    if (jsonBody.journalpostID === "999") {
-      const message = "sendToRina: Simulated status 500 on journalpostID === 999";
-      console.log(message);
-      const melding = ERR.serverError500(req.urll);
-      return res.status(500).send(melding);
+    console.log('send:', jsonBody);
+    switch (jsonBody.journalpostID) {
+      case "999":
+        console.log("send: Simulated status 500 on journalpostID == 999");
+        return res.status(500).send(ERR.serverError500(req.urll));
+      case "888":
+        console.log("send: Simulated status 401 on journalpostID == 888");
+        return res.status(401).send(ERR.unauthorizedRequest401(req.url));
+      case "777":
+        console.log("send: Simulated status 400 on journalpostID == 777");
+        return res.status(400).send(ERR.badRequest400(req.url));
+      default:
+        const response = {};
+        console.log("send: return response= {}");
+        res.json(response);
     }
-    if (jsonBody.journalpostID === "888") {
-      const message = "sendToRina: Simulated status 401 on journalpostID === 888";
-      console.log(message);
-      const melding = ERR.unauthorizedRequest401(req.url);
-      return res.status(401).send(melding);
-    }
-    const response = {};
-    res.json(response);
   }
   catch (err) {
     console.log(err);
