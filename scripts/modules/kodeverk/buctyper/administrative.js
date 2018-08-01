b5acacc97d1bea719d63beef44a7dd9417320b17
Wilhelm <Wilhelm.Karlsen@nav.no>
2018-08-01 15:32:09 +0200

    Rettet en mindre feil "->'

--- scripts/modules/kodeverk/buctyper/administrative.js
+++ scripts/modules/kodeverk/buctyper/administrative.js
@@ -1,51 +1,51 @@
 const administrative  = [
   {
-    kode: "AD_BUC_01",
-    term: "Close Case"
+    kode: 'AD_BUC_01',
+    term: 'Close Case'
   },
   {
-    kode: "AD_BUC_02",
-    term: "Reopen Case"
+    kode: 'AD_BUC_02',
+    term: 'Reopen Case'
   },
   {
-    kode: "AD_BUC_03",
-    term: "Add Participant"
+    kode: 'AD_BUC_03',
+    term: 'Add Participant'
   },
   {
-    kode: "AD_BUC_04",
-    term: "Remove Participant"
+    kode: 'AD_BUC_04',
+    term: 'Remove Participant'
   },
   {
-    kode: "AD_BUC_05",
-    term: "Forward Case"
+    kode: 'AD_BUC_05',
+    term: 'Forward Case'
   },
   {
-    kode: "AD_BUC_06",
-    term: "Invalidate SED"
+    kode: 'AD_BUC_06',
+    term: 'Invalidate SED'
   },
   {
-    kode: "AD_BUC_07",
-    term: "Reminder"
+    kode: 'AD_BUC_07',
+    term: 'Reminder'
   },
   {
-    kode: "AD_BUC_08",
-    term: "Clarify SED Content"
+    kode: 'AD_BUC_08',
+    term: 'Clarify SED Content'
   },
   {
-    kode: "AD_BUC_09",
-    term: "Reject SED"
+    kode: 'AD_BUC_09',
+    term: 'Reject SED'
   },
   {
-    kode: "AD_BUC_10",
-    term: "Update SED"
+    kode: 'AD_BUC_10',
+    term: 'Update SED'
   },
   {
-    kode: "AD_BUC_11",
-    term: "Business Exception"
+    kode: 'AD_BUC_11',
+    term: 'Business Exception'
   },
   {
-    kode: "AD_BUC_12",
-    term: "Change of Participant"
+    kode: 'AD_BUC_12',
+    term: 'Change of Participant'
   },
  ];
 exports.administrative = administrative;
--- scripts/modules/kodeverk/buctyper/recovery.js
+++ scripts/modules/kodeverk/buctyper/recovery.js
@@ -1,31 +1,31 @@
 const recovery = [
   {
-    kode: "RE_BUC_01",
-    term: "Motregning av for mye utbetalte ytelser"
+    kode: 'RE_BUC_01',
+    term: 'Motregning av for mye utbetalte ytelser'
   },
   {
-    kode: "RE_BUC_02",
-    term: "Motregning av for mye utbetalt ved restanser på utbetalingene"
+    kode: 'RE_BUC_02',
+    term: 'Motregning av for mye utbetalt ved restanser på utbetalingene'
   },
   {
-    kode: "RE_BUC_03",
-    term: "Motregning av midlertidig utbetalte ytelser med betaling fra annen medlemsstat jamfør art. 73 (1) i forordning 987/2009"
+    kode: 'RE_BUC_03',
+    term: 'Motregning av midlertidig utbetalte ytelser med betaling fra annen medlemsstat jamfør art. 73 (1) i forordning 987/2009'
   },
   {
-    kode: "RE_BUC_04",
-    term: "Offsetting of provisionally paid contributions"
+    kode: 'RE_BUC_04',
+    term: 'Offsetting of provisionally paid contributions'
   },
   {
-    kode: "RE_BUC_05",
-    term: "Anmodning om informasjon jamfør artikkel 76 i forordning (EF) nr. 987/2009"
+    kode: 'RE_BUC_05',
+    term: 'Anmodning om informasjon jamfør artikkel 76 i forordning (EF) nr. 987/2009'
   },
   {
-    kode: "RE_BUC_06",
-    term: "Anmodning om varsel jamfør artikkel 77 i forordning (EF) nr. 987/2009"
+    kode: 'RE_BUC_06',
+    term: 'Anmodning om varsel jamfør artikkel 77 i forordning (EF) nr. 987/2009'
   },
   {
-    kode: "RE_BUC_07",
-    term: "Krav om inndrivelse"
+    kode: 'RE_BUC_07',
+    term: 'Krav om inndrivelse'
   },
 ];
 exports.recovery = recovery;
