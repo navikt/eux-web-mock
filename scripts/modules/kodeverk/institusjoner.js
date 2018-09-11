459f20bbcf694b13b1e1cfdaa75f35bc9bd23268
Wilhelm Karlsen <Wilhelm.Karlsen@nav.no>
2018-09-11 12:18:47 +0200

    Updated expected institutions format.

--- scripts/modules/kodeverk/institusjoner.js
+++ scripts/modules/kodeverk/institusjoner.js
@@ -2,22 +2,57 @@ const institusjoner = [
   {
     kode: 'NAVT001',
     term: 'NAVT001',
+    bucs: [
+      "FB_BUC_01",
+      "P_BUC_01",
+      "AW_BUC_12",
+      "LA_BUC_01",
+      "AW_BUC_10b",
+    ]
   },
   {
     kode: 'NAVT002',
     term: 'NAVT002',
+    bucs: [
+      "FB_BUC_01",
+      "P_BUC_01",
+      "AW_BUC_12",
+      "LA_BUC_01",
+      "AW_BUC_10b",
+    ]
   },
   {
     kode: 'NAVT003',
     term: 'NAVT003',
+    bucs: [
+      "FB_BUC_01",
+      "P_BUC_01",
+      "AW_BUC_12",
+      "LA_BUC_01",
+      "AW_BUC_10b",
+    ]
   },
   {
     kode: 'dummy',
     term: 'dummy',
+    bucs: [
+      "FB_BUC_01",
+      "P_BUC_01",
+      "AW_BUC_12",
+      "LA_BUC_01",
+      "AW_BUC_10b",
+    ]
   },
   {
     kode: 'NAV Test1',
     term: 'NAV Test1',
+    bucs: [
+      "FB_BUC_01",
+      "P_BUC_01",
+      "AW_BUC_12",
+      "LA_BUC_01",
+      "AW_BUC_10b",
+    ]
   },
 ];
 
