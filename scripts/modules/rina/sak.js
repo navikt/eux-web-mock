595590a466d9fd390c2eb1a72a4a4507304895a0
Bjølseth, Are <are.bjolseth@nav.no>
2019-08-26 14:16:11 +0200

    Updated to valid rinasaksnummer, '161007', || '268016'

--- scripts/modules/rina/sak.js
+++ scripts/modules/rina/sak.js
@@ -6,7 +6,7 @@ const { moduleName } = Katalog.pathnameMap['rina-sak'];
 module.exports.sendSak = (req, res) => {
   setTimeout(() => {
     const response = {
-      rinasaksnummer: '12345678h9',
+      rinasaksnummer: '161007', // || '268016'
       vedleggID: '4ba46d7c7ef94f0e85cd0e4fae98da7d',
       // url: 'http://e34apvw004.devillo.no/portal/#/caseManagement/121788'
       url: 'http://www.nav.no',
